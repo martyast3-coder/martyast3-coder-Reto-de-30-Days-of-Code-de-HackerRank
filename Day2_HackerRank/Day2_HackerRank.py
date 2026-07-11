14243dd24c0410c46910e3d018ebf123520beb42
Marty Ramírez <martyast3@gmail.com>
2026-07-11 01:36:47 +0000

    Day_2

---
+++ Day2_HackerRank/Day2_HackerRank.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def solve(meal_cost, tip_percent, tax_percent):
+    tip = meal_cost * tip_percent / 100
+    tax = meal_cost * tax_percent / 100
+
+    total = meal_cost + tip + tax
+
+    print(round(total))
+
+if __name__ == '__main__':
+    meal_cost = float(input().strip())
+
+    tip_percent = int(input().strip())
+
+    tax_percent = int(input().strip())
+
+    solve(meal_cost, tip_percent, tax_percent)
