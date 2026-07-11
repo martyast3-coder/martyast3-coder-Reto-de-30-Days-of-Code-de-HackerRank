c35497874082a93bc1d6d63f791f223560d17a1c
Marty Ramírez <martyast3@gmail.com>
2026-07-11 15:08:39 +0000

    Actualizando del ¨Day0¨

--- hackerRank/day0/hackerRank/day0.py
+++ hackerRank/day0/hackerRank/day0.py
@@ -1,9 +1,9 @@
 # Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-Message_usuario= input('por favor escriba lo que necesita')
+Message_usuario= input('Muy Buenas tardes Srs.por favor digite su nombre o su Nombre de cita:')
 
 
 # Print a string literal saying "Hello, World." to stdout.
-print('Yo quiero una pizza de peperoni')
+print('Nosotros somos una empresa de tecnologia lo cual estamos especializados en la creacion de software y aplicaciones moviles, por lo cual nos gustaria saber que mensaje desea enviarnos:')
 
 # TODO: Write a line of code here that prints the contents of input_string to stdout.
-print(f'Este mensaje digitado por el usuario es:(Yo quiero una pizza de peperoni)')
+print(f'(Pase un excelente dia) Srs {Message_usuario}')
