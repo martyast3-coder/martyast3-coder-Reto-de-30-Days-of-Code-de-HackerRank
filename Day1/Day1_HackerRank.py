cf2b9ef3080a38869f87503acaecbe9fe7f58ec6
Marty Ramírez <martyast3@gmail.com>
2026-07-11 00:27:11 +0000

    Day_1_HackerRank

---
+++ Day1/Day1_HackerRank.py
@@ -0,0 +1,27 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+# Read input
+i = int(input())
+d = float(input())
+s = input()
+
+# Initial values
+i0 = 4
+d0 = 4.0
+s0 = "HackerRank "
+
+# Print results
+print(i0 + i)
+print(d0 + d)
+print(s0 + s)
