c1e8e472d5b50ca564d9f573e53b346f51c5e522
Marty Ramírez <martyast3@gmail.com>
2026-07-13 23:48:08 +0000

    Actualizacion de reto day 1

--- hackerRank./Day1_HackerRank/Day1_HackerRank.py
+++ hackerRank./Day1_HackerRank/Day1_HackerRank.py
@@ -1,27 +1,22 @@
 i = 4
 d = 4.0
 s = 'HackerRank '
+
 # Declare second integer, double, and String variables.
+i2 = 0
+d2 = 0.0
+s2 = ""
 
 # Read and save an integer, double, and String to your variables.
+i2 = int(input())
+d2 = float(input())
+s2 = input()
 
 # Print the sum of both integer variables on a new line.
+print(i + i2)
 
 # Print the sum of the double variables on a new line.
+print(d + d2)
 
-# Concatenate and print the String variables on a new line
-# The 's' variable above should be printed first.
-# Read input
-i = int(input())
-d = float(input())
-s = input()
-
-# Initial values
-i0 = 4
-d0 = 4.0
-s0 = "HackerRank "
-
-# Print results
-print(i0 + i)
-print(d0 + d)
-print(s0 + s)
+# Concatenate and print the String variables on a new line.
+print(s + s2)
