9053b71b1dda576a6fb3c582db14cadfaf67587e
Marty Ramírez <martyast3@gmail.com>
2026-07-18 14:04:31 +0000

    Actualizacion

--- hackerRank/Day1_HackerRank/Day1_HackerRank.py
+++ hackerRank/Day1_HackerRank/Day1_HackerRank.py
@@ -1,22 +1,15 @@
+# Variables iniciales
 i = 4
 d = 4.0
-s = 'HackerRank '
+s = "HackerRank "
 
-# Declare second integer, double, and String variables.
-i2 = 0
-d2 = 0.0
-s2 = ""
+# Pedir los datos al usuario
+Numero_entero = int(input("Ingrese un número entero: "))
+Numero_decimal = float(input("Ingrese un número decimal: "))
+Mensaje_usuario = input("Ingrese un mensaje: ")
 
-# Read and save an integer, double, and String to your variables.
-i2 = int(input())
-d2 = float(input())
-s2 = input()
-
-# Print the sum of both integer variables on a new line.
-print(i + i2)
-
-# Print the sum of the double variables on a new line.
-print(d + d2)
-
-# Concatenate and print the String variables on a new line.
-print(s + s2)
+# Mostrar los resultados
+print("\nResultados:")
+print("Suma de enteros:", i + Numero_entero)
+print("Suma de decimales:", d + Numero_decimal)
+print("Concatenación:", s + Mensaje_usuario)
